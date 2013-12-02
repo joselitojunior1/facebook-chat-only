7edd052b2e2cb8d4893e021a93427fbd7aa9508c
Lucas Inojosa <lucas@atepassar.com>
2013-12-02 17:32:03 -0300

    Hiding elements instead of excluding them

--- chat.js
+++ chat.js
@@ -1,5 +1,19 @@
-for (elementid in {'globalContainer':'','pagelet_bluebar':'','pagelet_ticker':''}) {
-    var element = document.getElementById(elementid);
-    element.parentNode.removeChild(element);
-}
-document.title = "Facebook Chat";
+var globalContainer = document.getElementById('globalContainer');
+var pagelet_bluebar = document.getElementById('pagelet_bluebar');
+var pagelet_ticker = document.getElementById('pagelet_bluebar');
+
+var background = document.createElement('div');
+background.id = "blank_div";
+background.style.position = 'absolute';
+background.style.width = '100%';
+background.style.height = '100%';
+background.style.backgroundColor = '#FFFFFF';
+background.style.backgroundImage = "url('background.jpg')";
+background.style.zIndex = '1';
+
+globalContainer.parentNode.insertBefore(background, globalContainer);
+globalContainer.style.display = 'none';
+pagelet_bluebar.style.display = 'none';
+pagelet_ticker.style.display = 'none';
+
+document.title = 'Facebook Chat';
