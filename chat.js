31267a5d125eeb159c43b07c8f75164070a5e4ec
Lucas Inojosa <lucas@atepassar.com>
2013-12-03 12:53:41 -0300

    Updates' div on dock now hidden

--- chat.js
+++ chat.js
@@ -1,19 +1,40 @@
 var globalContainer = document.getElementById('globalContainer');
+globalContainer.style.display = 'none';
+
 var pagelet_bluebar = document.getElementById('pagelet_bluebar');
-var pagelet_ticker = document.getElementById('pagelet_bluebar');
+pagelet_bluebar.style.display = 'none';
+
+var pagelet_ticker = document.getElementById('pagelet_ticker');
+pagelet_ticker.style.display = 'none';
+
+function toArray(collection) {
+    var array = [];
+    for (var i = 0; i < collection.length; i++) {
+        array.push(collection[i]);
+    }
+    return array;
+}
+
+var pagelet_dock = document.getElementById('pagelet_dock');
+var updates_container = toArray(pagelet_dock.children).filter(function(elem) {
+    return toArray(elem.children).filter(function(elem) {
+        return elem.tagName === 'UL';
+    }).length > 0;
+});
+
+for (var i = 0; i < updates_container.length; i++) {
+    console.log(updates_container[i].tagName);
+    updates_container[i].style.display = 'none';
+}
 
 var background = document.createElement('div');
 background.id = "blank_div";
 background.style.position = 'absolute';
 background.style.width = '100%';
 background.style.height = '100%';
-background.style.backgroundColor = '#FFFFFF';
-background.style.backgroundImage = "url('background.jpg')";
+background.style.backgroundColor = '#EEEEEE';
+//background.style.backgroundImage = "url('background.jpg')";
 background.style.zIndex = '1';
-
 globalContainer.parentNode.insertBefore(background, globalContainer);
-globalContainer.style.display = 'none';
-pagelet_bluebar.style.display = 'none';
-pagelet_ticker.style.display = 'none';
 
 document.title = 'Facebook Chat';
