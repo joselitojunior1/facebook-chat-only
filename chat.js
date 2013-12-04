1c319295ac5477496199bef895a232e7b568c909
Lucas Inojosa <lucas@atepassar.com>
2013-12-04 11:23:22 -0300

    Activate/Deactivate chat-only

--- chat.js
+++ chat.js
@@ -1,12 +1,3 @@
-var globalContainer = document.getElementById('globalContainer');
-globalContainer.style.display = 'none';
-
-var pagelet_bluebar = document.getElementById('pagelet_bluebar');
-pagelet_bluebar.style.display = 'none';
-
-var pagelet_ticker = document.getElementById('pagelet_ticker');
-pagelet_ticker.style.display = 'none';
-
 function toArray(collection) {
     var array = [];
     for (var i = 0; i < collection.length; i++) {
@@ -15,25 +6,49 @@ function toArray(collection) {
     return array;
 }
 
-var pagelet_dock = document.getElementById('pagelet_dock');
-var updates_container = toArray(pagelet_dock.children).filter(function(elem) {
-    return toArray(elem.children).filter(function(elem) {
-        return elem.tagName === 'UL';
-    }).length > 0;
-});
+function toggleElements(styleDisplay) {
+    var globalContainer = document.getElementById('globalContainer');
+    globalContainer.style.display = styleDisplay;
 
-for (var i = 0; i < updates_container.length; i++) {
-    updates_container[i].style.display = 'none';
+    var pagelet_bluebar = document.getElementById('pagelet_bluebar');
+    pagelet_bluebar.style.display = styleDisplay;
+
+    var pagelet_ticker = document.getElementById('pagelet_ticker');
+    pagelet_ticker.style.display = styleDisplay;
+
+    var pagelet_dock = document.getElementById('pagelet_dock');
+    var updates_container = toArray(pagelet_dock.children).filter(function(elem) {
+        return toArray(elem.children).filter(function(elem) {
+            return elem.tagName === 'UL';
+        }).length > 0;
+    });
+
+    for (var i = 0; i < updates_container.length; i++) {
+        updates_container[i].style.display = styleDisplay;
+    }
 }
 
-var background = document.createElement('div');
-background.id = "blank_div";
-background.style.position = 'absolute';
-background.style.width = '100%';
-background.style.height = '100%';
-background.style.backgroundColor = '#EEEEEE';
-//background.style.backgroundImage = "url('background.jpg')";
-background.style.zIndex = '1';
-globalContainer.parentNode.insertBefore(background, globalContainer);
+if (window.chatOnlyActive === undefined) {
+    window.chatOnlyActive = false;
+}
 
-document.title = 'Facebook Chat';
+if (!window.chatOnlyActive) {
+    toggleElements('none');
+    var background = document.createElement('div');
+    background.id = "blank_div";
+    background.style.position = 'absolute';
+    background.style.width = '100%';
+    background.style.height = '100%';
+    background.style.backgroundColor = '#EEEEEE';
+    //background.style.backgroundImage = "url('background.jpg')";
+    background.style.zIndex = '1';
+    globalContainer.parentNode.insertBefore(background, globalContainer);
+    document.title = 'Facebook Chat';
+    window.chatOnlyActive = true;
+} else {
+    toggleElements('');
+    var background = document.getElementById('blank_div');
+    background.parentNode.removeChild(background);
+    document.title = 'Facebook';
+    window.chatOnlyActive = false;
+}
