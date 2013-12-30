6f0762721ca23a629ebd81b28254be3ed835c005
Joselito Júnior <eu@joselitojunior.com>
2013-12-29 23:49:41 -0300

    Apagando imagens desnecessárias e melhorando a visualização das mensagens
    A barra superior contem apenas o icone de mensagens quando a extensão é
    ativada. Isso é util para visualizar mensagens de chats não-ativos ou
    de pessoas bloqueadas/desabilitadas/que não são seus amigos no Facebook

--- chat.js
+++ chat.js
@@ -5,17 +5,46 @@ function toArray(collection) {
 function toggleElements(activate) {
     var styleDisplay = activate ? 'none' : '';
     var styleDisplayBackground = !activate ? 'none' : '';
+    var marginDisplay = activate ? '148px' : '0';
 
     var globalContainer = document.getElementById('globalContainer');
     globalContainer.style.display = styleDisplay;
 
-    var pageletBluebar = document.getElementById('pagelet_bluebar');
-    pageletBluebar.style.display = styleDisplay;
+    /*var pageletBluebar = document.getElementById('pagelet_bluebar');
+    pageletBluebar.style.display = styleDisplay;*/
 
     var pageletTicker = document.getElementById('pagelet_ticker');
     pageletTicker.style.display = styleDisplay;
 
+    var pageLogo = document.getElementById('pageLogo');
+    pageLogo.style.display = styleDisplay;
+
+    var rSearch = document.querySelector('[role="search"]');
+    rSearch.style.display = styleDisplay;
+
+    var fbRequestsJewel = document.getElementById('fbRequestsJewel');
+    fbRequestsJewel.style.display = styleDisplay;
+
+    var fbNotificationsJewel = document.getElementById('fbNotificationsJewel');
+    fbNotificationsJewel.style.display = styleDisplay;
+
+    var navHome = document.getElementById('navHome');
+    navHome.style.display = styleDisplay;
+    
+    var navTimeline = document.getElementById('navTimeline');
+    navTimeline.style.display = styleDisplay;
+    
+    var navPrivacy = document.getElementById('navPrivacy');
+    navPrivacy.style.display = styleDisplay;
+    
+    var navAccount = document.getElementById('navAccount');
+    navAccount.style.display = styleDisplay;
+
+    var fbMessagesJewel = document.getElementById('fbMessagesJewel');
+    fbMessagesJewel.style.marginRight = marginDisplay;
+
     var pageletDock = document.getElementById('pagelet_dock');
+
     var updatesContainer = toArray(pageletDock.children).filter(function(elem) {
         return toArray(elem.children).filter(function(elem) {
             return elem.tagName === 'UL';
@@ -40,7 +69,7 @@ if (window.chatOnlyBackground === undefined) {
     window.chatOnlyBackground.style.height = '100%';
     window.chatOnlyBackground.style.backgroundColor = '#6286D2';
     window.chatOnlyBackground.style.WebkitOpacity = '0.3';
-    window.chatOnlyBackground.style.zIndex = '1';
+    window.chatOnlyBackground.style.zIndex = '0';
 
     var globalContainer = document.getElementById('globalContainer');
     globalContainer.parentNode.insertBefore(window.chatOnlyBackground, globalContainer);
